24f4381922966f99015e0a729c86236c26e5bab1
smsohag32 <sohagsheik32@gmail.com>
2025-02-19 23:04:59 +0600

    added textarea

--- src/components/CodeGenerate/CodeGenerate.tsx
+++ src/components/CodeGenerate/CodeGenerate.tsx
@@ -1,10 +1,14 @@
-/* eslint-disable no-useless-escape */
+"use client";
+
+import type React from "react";
+
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { CodeType, InputType } from "@/types/index";
+import { Textarea } from "@/components/ui/textarea";
+import type { CodeType, InputType } from "@/types/index";
 import { Barcode, QrCode, Globe, Mail, Phone, Wifi, AlertCircle, Eye, EyeOff } from "lucide-react";
 import { ColorPicker } from "../ui/color-picker";
 
@@ -61,7 +65,7 @@ export function CodeGenerator({
    const validateInput = () => {
       switch (inputType) {
          case "url":
-            return /^(https?:\/\/)?(www\.)?([a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]+)+)(\/[^\s]*)?$/.test(
+            return /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+)(\/[^\s]*)?$/.test(
                input
             )
                ? null
@@ -87,7 +91,6 @@ export function CodeGenerator({
       return null;
    };
 
-   // hangle generate
    const handleSubmit = async (e: React.FormEvent) => {
       e.preventDefault();
       const validationError = validateInput();
@@ -130,18 +133,32 @@ export function CodeGenerator({
                {inputType.charAt(0).toUpperCase() + inputType.slice(1)}
             </Label>
             <div className="relative">
-               <Input
-                  id="code-input"
-                  type={inputType === "email" ? "email" : inputType === "tel" ? "tel" : "text"}
-                  placeholder={inputTypeConfig[inputType].placeholder}
-                  value={input}
-                  onChange={(e) => {
-                     setInput(e.target.value);
-                     setError(null);
-                  }}
-                  className={`pl-10 py-2.5 ${error ? "border-red-500" : ""}`}
-               />
-               <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+               {inputType === "text" ? (
+                  <Textarea
+                     id="code-input"
+                     placeholder={inputTypeConfig[inputType].placeholder}
+                     value={input}
+                     rows={6}
+                     onChange={(e) => {
+                        setInput(e.target.value);
+                        setError(null);
+                     }}
+                     className={`pl-10 py-2.5 ${error ? "border-red-500" : ""}`}
+                  />
+               ) : (
+                  <Input
+                     id="code-input"
+                     type={inputType === "email" ? "email" : inputType === "tel" ? "tel" : "text"}
+                     placeholder={inputTypeConfig[inputType].placeholder}
+                     value={input}
+                     onChange={(e) => {
+                        setInput(e.target.value);
+                        setError(null);
+                     }}
+                     className={`pl-10 py-2.5 ${error ? "border-red-500" : ""}`}
+                  />
+               )}
+               <div className="absolute left-3 top-3 text-gray-400">
                   {inputTypeConfig[inputType].icon}
                </div>
             </div>
@@ -210,8 +227,6 @@ export function CodeGenerator({
                {isLoading ? "Generating..." : "Generate Code"}
             </Button>
          </div>
-
-         {/* customize style  */}
 
          <div className="mt-6  rounded-sm border-slate-200">
             <p>Customize Styles</p>
